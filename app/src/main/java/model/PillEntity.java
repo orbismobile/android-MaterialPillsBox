461c5d5de29bac56d7e1b813ef9f6db149d8e74f
Carlos Leonardo Camilo Vargas Huamán <carlosleonardovargashuaman@gmail.com>
2017-05-20 12:43:07 -0500

    Add PillEntity class :beers:

---
+++ app/src/main/java/model/PillEntity.java
@@ -0,0 +1,23 @@
+package model;
+
+/**
+ * Created by Carlos Leonardo Camilo Vargas Huamán on 5/20/17.
+ *
+ */
+
+public class PillEntity {
+
+    private String name;
+
+    public PillEntity(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- app/src/main/java/pe/elcomercio/materialpills/MainActivity.java
+++ app/src/main/java/pe/elcomercio/materialpills/MainActivity.java
@@ -1,31 +1,19 @@
 package pe.elcomercio.materialpills;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.Button;
 
-import java.util.ArrayList;
-import java.util.List;
 
-import pe.elcomercio.materialpillsbox.MaterialPillsBox;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-public class MainActivity extends AppCompatActivity {
-
-    MaterialPillsBox materialPillsBox;
-    LayoutInflater mInflater;
-    private List<String> pillNamesList = new ArrayList<>();
+    private Button btnNormal;
+    private Button btnDelete;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,41 +21,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        materialPillsBox = (MaterialPillsBox) findViewById(R.id.mtbArea);
-        mInflater = LayoutInflater.from(this);
-
-        for (int i = 0; i < 10; i++) {
-            pillNamesList.add("Init " + i);
-        }
-
-        for (int i = 0; i < pillNamesList.size(); i++) {
-            initFlowlayout2(i);
-        }
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                pillNamesList.add("New " + pillNamesList.size());
-                initFlowlayout2(pillNamesList.size()-1);
-                Log.e("ADDED", "TAG " + pillNamesList.get(pillNamesList.size()-1));
-            }
-        });
-    }
-
-    public void initFlowlayout2(int i) {
-        final LinearLayout linear = (LinearLayout) mInflater.inflate(R.layout.pills_box_layout, materialPillsBox, false);
-        TextView tv1 = (TextView) linear.findViewById(R.id.lblPill);
-        tv1.setText(pillNamesList.get(i));
-        linear.setTag(i);
-        materialPillsBox.addView(linear);
-        linear.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.e("REMOVED", "TAG " + linear.getTag());
-                materialPillsBox.removeView(v);
-            }
-        });
+        btnNormal = (Button) findViewById(R.id.btnNormal);
+        btnDelete = (Button) findViewById(R.id.btnDelete);
+        btnNormal.setOnClickListener(this);
+        btnDelete.setOnClickListener(this);
     }
 
     @Override
@@ -90,5 +47,17 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnNormal:
+                startActivity(new Intent(MainActivity.this, NormalPillsActivity.class));
+                break;
+            case R.id.btnDelete:
+                startActivity(new Intent(MainActivity.this, DeletePillsActivity.class));
+                break;
+        }
     }
 }
