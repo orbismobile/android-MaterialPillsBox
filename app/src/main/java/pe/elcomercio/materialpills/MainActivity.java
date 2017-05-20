7d184344af506e66f8bcbc3ff9afbe06fd752d6a
Carlos Leonardo Camilo Vargas Huamán <carlosleonardovargashuaman@gmail.com>
2017-05-19 19:10:18 -0500

    WORKING

--- app/src/main/java/pe/elcomercio/materialpills/MainActivity.java
+++ app/src/main/java/pe/elcomercio/materialpills/MainActivity.java
@@ -4,28 +4,90 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import pe.elcomercio.materialpillsbox.MaterialPillsBox;
 
 public class MainActivity extends AppCompatActivity {
 
-    SearchView
+    MaterialPillsBox materialPillsBox;
+    private List<String> pillNamesList = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        materialPillsBox = (MaterialPillsBox) findViewById(R.id.mtbArea);
+
+
+        pillNamesList.add("CarlitosMB");
+        pillNamesList.add("CarlitosDroid");
+        pillNamesList.add("Jan");
+        pillNamesList.add("Ricardo");
+        pillNamesList.add("Carlos");
+
+
+        for (int i = 0; i < pillNamesList.size(); i++) {
+            final LinearLayout linearLayout = (LinearLayout) LayoutInflater.from(this).inflate(R.layout.pills_box_layout, materialPillsBox, false);
+            ImageView imageView = (ImageView) linearLayout.findViewById(R.id.imgClose);
+            TextView textView = (TextView) linearLayout.findViewById(R.id.lblPill);
+            textView.setText(pillNamesList.get(i));
+            linearLayout.setTag(i);
+            textView.setTag(i);
+            materialPillsBox.addView(linearLayout);
+
+
+            textView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int i = (int) v.getTag();
+                    View view = materialPillsBox.getChildAt(i);
+                    Toast.makeText(MainActivity.this, ""+ i , Toast.LENGTH_SHORT).show();
+                    materialPillsBox.removeView(view);
+
+
+                }
+            });
+
+        }
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                pillNamesList.add("Nuevo ");
+
+                final LinearLayout aloha = (LinearLayout) LayoutInflater.from(MainActivity.this).inflate(R.layout.pills_box_layout, materialPillsBox, false);
+                TextView textView = (TextView) aloha.findViewById(R.id.lblPill);
+                textView.setText(pillNamesList.get(pillNamesList.size() -1));
+                aloha.setTag(pillNamesList.size());
+                textView.setTag(pillNamesList.size());
+                materialPillsBox.addView(aloha);
+
+                textView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        int i = (int) v.getTag();
+                        View view = materialPillsBox.getChildAt(i);
+                        Toast.makeText(MainActivity.this, ""+ i , Toast.LENGTH_SHORT).show();
+                        materialPillsBox.removeView(view);
+
+                        Toast.makeText(MainActivity.this, "CARLITOS  "  +  aloha.getTag(), Toast.LENGTH_SHORT).show();
+                    }
+                });
             }
         });
     }
--- materialpillsbox/src/main/java/pe/elcomercio/materialpillsbox/MaterialPillsBox.java
+++ materialpillsbox/src/main/java/pe/elcomercio/materialpillsbox/MaterialPillsBox.java
@@ -1,47 +1,164 @@
 package pe.elcomercio.materialpillsbox;
 
 import android.content.Context;
-import android.content.res.TypedArray;
-import android.support.v7.widget.TintTypedArray;
 import android.util.AttributeSet;
-import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Carlos Leonardo Camilo Vargas Huamán on 5/18/17.
- *
  */
 
-public class MaterialPillsBox extends ViewGroup{
+public class MaterialPillsBox extends ViewGroup {
 
     public MaterialPillsBox(Context context) {
         super(context);
-        init(context, null);
     }
 
     public MaterialPillsBox(Context context, AttributeSet attrs) {
         super(context, attrs);
-        init(context, attrs);
     }
 
     public MaterialPillsBox(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        init(context, attrs);
-    }
-
-    public void init(Context context, AttributeSet attrs){
-        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.MaterialPillsBox);
-        final LayoutInflater inflater = LayoutInflater.from(context);
-
-
     }
 
 
     @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+
+        int sizeWidth = MeasureSpec.getSize(widthMeasureSpec);
+        int modeWidth = MeasureSpec.getMode(widthMeasureSpec);
+        int sizeHeight = MeasureSpec.getSize(heightMeasureSpec);
+        int modeHeight = MeasureSpec.getMode(heightMeasureSpec);
+
+        int width = 0;
+        int height = 0;
+
+        int lineWidth = 0;
+        int lineHeight = 0;
+
+        int cCount = getChildCount();
+
+        for (int i = 0; i < cCount; i++) {
+            View child = getChildAt(i);
+
+            measureChild(child, widthMeasureSpec, heightMeasureSpec);
+
+            MarginLayoutParams lp = (MarginLayoutParams) child.getLayoutParams();
+
+
+            int childWidth = child.getMeasuredWidth() + lp.leftMargin + lp.rightMargin;
+            int childHeight = child.getMeasuredHeight() + lp.topMargin + lp.bottomMargin;
+
+            if (lineWidth + childWidth > sizeWidth - getPaddingLeft() - getPaddingRight()) {
+                width = Math.max(width, lineWidth);
+                lineWidth = childWidth;
+                height += lineHeight;
+                lineHeight = childHeight;
+            } else {
+                lineWidth += childWidth;
+                lineHeight = Math.max(lineHeight, childHeight);
+            }
+            if (i == cCount - 1) {
+                width = Math.max(lineWidth, width);
+                height += lineHeight;
+            }
+        }
+
+
+        setMeasuredDimension(
+                modeWidth == MeasureSpec.EXACTLY ? sizeWidth : width + getPaddingLeft() + getPaddingRight(),
+                modeHeight == MeasureSpec.EXACTLY ? sizeHeight : height + getPaddingTop() + getPaddingBottom()
+        );
+
+    }
+
+
+    private List<List<View>> mAllViews = new ArrayList<List<View>>();
+    private List<Integer> mLineHeight = new ArrayList<Integer>();
+
+    @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        mAllViews.clear();
+        mLineHeight.clear();
+
+        int width = getWidth();
+
+        int lineWidth = 0;
+        int lineHeight = 0;
+        List<View> lineViews = new ArrayList<View>();
+
+        int cCount = getChildCount();
+
+        for (int i = 0; i < cCount; i++) {
+            View child = getChildAt(i);
+            MarginLayoutParams lp = (MarginLayoutParams) child.getLayoutParams();
+
+            int childWidth = child.getMeasuredWidth();
+            int childHeight = child.getMeasuredHeight();
 
 
+            if (lineWidth + childWidth + lp.leftMargin + lp.rightMargin > width - getPaddingLeft() - getPaddingRight()) {
+                mLineHeight.add(lineHeight);
+                mAllViews.add(lineViews);
+
+                lineWidth = 0;
+                lineHeight = childHeight + lp.topMargin + lp.bottomMargin;
+                lineViews = new ArrayList<View>();
+            }
+
+            lineWidth += childWidth + lp.leftMargin + lp.rightMargin;
+            lineHeight = Math.max(lineHeight, childHeight + lp.topMargin + lp.bottomMargin);
+            lineViews.add(child);
 
 
+        }
+
+        mLineHeight.add(lineHeight);
+        mAllViews.add(lineViews);
+
+
+        int left = getPaddingLeft();
+        int top = getPaddingTop();
+
+        int lineNum = mAllViews.size();
+
+        for (int i = 0; i < lineNum; i++) {
+            lineViews = mAllViews.get(i);
+            lineHeight = mLineHeight.get(i);
+
+            for (int j = 0; j < lineViews.size(); j++) {
+                View child = lineViews.get(j);
+                if (child.getVisibility() == View.GONE) {
+                    continue;
+                }
+
+                MarginLayoutParams lp = (MarginLayoutParams) child.getLayoutParams();
+
+                int lc = left + lp.leftMargin;
+                int tc = top + lp.topMargin;
+                int rc = lc + child.getMeasuredWidth();
+                int bc = tc + child.getMeasuredHeight();
+
+                child.layout(lc, tc, rc, bc);
+
+                left += child.getMeasuredWidth() + lp.leftMargin + lp.rightMargin;
+            }
+            left = getPaddingLeft();
+            top += lineHeight;
+        }
+
+    }
+
+    @Override
+    public LayoutParams generateLayoutParams(AttributeSet attrs) {
+        return new MarginLayoutParams(getContext(), attrs);
     }
 }
